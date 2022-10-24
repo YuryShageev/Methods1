c4748bf1d58ed7ed1bd7731175707e3e07c69e5e
YuryShageev <schageev.yur@yandex.ru>
2022-10-24 15:18:51 +0300

    Разбор и переделка методов. Последнее не доходит

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Main {
 
 
@@ -11,10 +13,64 @@ public class Main {
         }
         return 0;
     }
+
+    public static int findOutVersion(int a) {
+        if (a == 1) {
+            System.out.println("Ваша операционная система - Android");
+        } else {
+            System.out.println("Ваша операционная система - iOS");
+        }
+        return 0;
+    }
+
+    public static int findOutPhoneRelease(int a) {
+        if (a >= 2015) {
+            System.out.println("Ваша система современная");
+        } else {
+            System.out.println("Установите облегченную версию программы");
+        }
+        return 0;
+    }
+
+    public static void showResult(int a) {
+        System.out.println();
+    }
+
+    public static int findDeliveryDistance(int b) {
+        for (b = 0; b < 10; b++) {
+        }
+        return 0;
+    }
+
+    public static int understandDistance(int a, int b) {
+        b = findDeliveryDistance(b);
+        if (a < 20){
+            System.out.println("Потребуется " + b + " день");
+        } else if (a >= 20 || a < 60) {
+            System.out.println("Потребуется " + b + " дня");
+        } else {
+            System.out.println("Потребуется больше 3-х дней");
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Task 1");
         int year = 0;
         checkLeapYear(year);
+
+        int clientOS = 1;
+        findOutVersion(clientOS);
+
+        int phoneVersion = LocalDate.now().getYear();
+        findOutPhoneRelease(phoneVersion);
+
+        int deliveryDistance = 95;
+        int numberDays = 0;
+        understandDistance(deliveryDistance, numberDays);
+
     }
+
+
 }

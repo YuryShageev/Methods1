28bf656eebdc9da1769b3018043e03463abb6fdc
YuryShageev <schageev.yur@yandex.ru>
2022-10-24 16:38:18 +0300

    Разбор и переделка методов. Последнее не доходит

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,23 @@ public class Main {
         return 0;
     }
 
+    public static void printIsLeapYear(int year) {
+        boolean yearIsLeap = isLeap(year);
+        printIsLeapYearResult(year, yearIsLeap);
+    }
+    private static boolean isLeap(int year){
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
+    private static void printIsLeapYearResult(int year, boolean yearIsLeap) {
+        if (yearIsLeap) {
+            System.out.println(year + " Високосный");
+        } else {
+            System.out.println(year + " Невисокосный");
+        }
+    }
+
+
     public static int findOutVersion(int a) {
         if (a == 1) {
             System.out.println("Ваша операционная система - Android");
@@ -32,9 +49,25 @@ public class Main {
         return 0;
     }
 
-    public static void showResult(int a) {
-        System.out.println();
+    public static void recommendApplicationVersion(int clientOS, int deviceYear) {
+        boolean deviseIsOld = isDeviceOld(deviceYear);
+        System.out.print("Установите ");
+        if (deviseIsOld) {
+            System.out.println("lite ");
+        }
+        System.out.print("версию для ");
+        if (clientOS == 0) {
+            System.out.println("iOS");
+        } else {
+            System.out.println("Android");
+        }
     }
+
+    private static boolean isDeviceOld(int deviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        return deviceYear <= currentYear;
+    }
+
 
     public static int findDeliveryDistance(int b) {
         for (b = 0; b < 10; b++) {
@@ -57,15 +90,20 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Task 1");
-        int year = 0;
+        int year = 2019;
         checkLeapYear(year);
+        printIsLeapYear(year);// Переделка первого задаия
 
+        System.out.println("Task 2");
         int clientOS = 1;
         findOutVersion(clientOS);
 
         int phoneVersion = LocalDate.now().getYear();
         findOutPhoneRelease(phoneVersion);
 
+        recommendApplicationVersion(clientOS, year);
+
+        System.out.println("Task 3");
         int deliveryDistance = 95;
         int numberDays = 0;
         understandDistance(deliveryDistance, numberDays);

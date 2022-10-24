57efbd44002f9c8118a15e84810ee8d34085cd2e
YuryShageev <schageev.yur@yandex.ru>
2022-10-24 11:52:39 +0300

    Разбор и переделка методов

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+
+
+    public static int checkLeapYear(int a) {
+        for (int i = 2020; i < 2050; i++) {
+            if (i % 4 == 0) {
+                System.out.println("Високосный год " + i);
+            } else {
+                System.out.println("Невисокосный год " + i);
+            }
+        }
+        return 0;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int year = 0;
+        checkLeapYear(year);
     }
 }

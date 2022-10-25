ecd91f2cb8ea562cc8c36d6fdaeb65e2c461749c
YuryShageev <schageev.yur@yandex.ru>
2022-10-25 12:01:51 +0300

    Разбор и переделка методов. Последнее не доходит

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,6 @@ import java.time.LocalDate;
 public class Main {
 
 
-    public static int checkLeapYear(int a) {
-        for (int i = 2020; i < 2050; i++) {
-            if (i % 4 == 0) {
-                System.out.println("Високосный год " + i);
-            } else {
-                System.out.println("Невисокосный год " + i);
-            }
-        }
-        return 0;
-    }
 
     public static void printIsLeapYear(int year) {
         boolean yearIsLeap = isLeap(year);
@@ -90,7 +80,7 @@ public class Main {
         if (a < 20){
             b++;
             System.out.println("Потребуется " + b + " день");
-        } else if (a >= 20 || a < 60) {
+        } else if (a >= 20 && a < 60) {
             b++;
             System.out.println("Потребуется " + b + " дня");
         } else {
@@ -117,7 +107,6 @@ public class Main {
 
         System.out.println("Task 1");
         int year = 2022;
-        checkLeapYear(year);
         printIsLeapYear(year);// Переделка первого задаия
 
         System.out.println("Task 2");
